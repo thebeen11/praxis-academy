a3aadadc0a3e215de366911c6cd2c92d1d7f0f92
thebeen11 <bagus11nugroho@gmail.com>
2019-09-03 15:26:22 +0700

    01-02

--- novice/01-02/kasus/kasus 1/main.js
+++ novice/01-02/kasus/kasus 1/main.js
@@ -1,13 +1,13 @@
 let x, y;
-let myHeading = document.querySelector('body');
+let myText = document.querySelector('body');
 
 x = window.prompt('masukkan angka pertama!', '0');
 y = window.prompt('masukkan angka kedua!', '0');
 
 if (x == y) {
-    myHeading.textContent = "nilai "+ x +" sama dengan " +y;
+    myText.textContent = "nilai "+ x +" sama dengan " +y;
   }else if(x < y){
-    myHeading.textContent = "nilai "+ x +" lebih kecil dari "+ y;
+    myText.textContent = "nilai "+ x +" lebih kecil dari "+ y;
   }else if(x > y){
-    myHeading.textContent = "nilai "+ x +" lebih besar dari "+ y;
+    myText.textContent = "nilai "+ x +" lebih besar dari "+ y;
   }
--- novice/01-02/kasus/kasus 2/main.js
+++ novice/01-02/kasus/kasus 2/main.js
@@ -1,24 +1,27 @@
 var x=3;
 var y=-7;
 var z=2;
+
+let myText = document.querySelector('body');
+
 if (x>0 && y>0 && z>0)
 {
-       alert("The sign is +");
+       alert("Tanda +");
 }
 else if (x<0 && y<0 && z<0)
         {
-          console.log("The sign is +");
+            myText.textContent = "Tanda +";
         }
         else if (x>0 && y<0 && z<0)
         {
-          console.log("The sign is +");
+            myText.textContent = "Tanda +";
         }
         else if (x<0 && y>0 && z<0)
         {
-          console.log("The sign is +");
+            myText.textContent = "Tanda +";
         }
         else
         {
-          console.log("The sign is -");
+            myText.textContent = "Tanda -";
         }
 		
