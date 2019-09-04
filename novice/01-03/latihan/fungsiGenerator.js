064d7dcba64c18e3c9101abfe1336f13b0d50763
thebeen11 <bagus11nugroho@gmail.com>
2019-09-04 16:57:51 +0700

    01-03

---
+++ novice/01-03/latihan/fungsiGenerator.js
@@ -0,0 +1,11 @@
+function* generator(i) {
+    yield i;
+    yield i + 10;
+  }
+  
+  var gen = generator(10);
+  
+  console.log(gen.next().value);
+
+  
+  console.log(gen.next().value);
---
+++ novice/01-03/latihan/hoisted.js
@@ -0,0 +1,5 @@
+function exampleScope() {
+    var foo;
+    foo = 'Hello, world!';
+  }
+  
