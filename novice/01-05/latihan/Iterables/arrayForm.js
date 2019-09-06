c4695d4c2f7e9712800cd0719edcb36e96fc6fcd
thebeen11 <bagus11nugroho@gmail.com>
2019-09-06 15:28:26 +0700

    01-05

---
+++ novice/01-05/latihan/Iterables/arrayForm.js
@@ -0,0 +1,8 @@
+let arrayLike = {
+    0: "Hello",
+    1: "World",
+    length: 2
+  };
+  
+  let arr = Array.from(arrayLike); 
+console.log(arr.pop());
---
+++ novice/01-05/latihan/Iterables/iteratorArray.js
@@ -0,0 +1,9 @@
+let arrayLike = { 
+    0: "Hello",
+    1: "World",
+    length: 2
+  };
+ 
+
+  console.log(arrayLike);
+  
