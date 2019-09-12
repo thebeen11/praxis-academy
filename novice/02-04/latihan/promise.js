ec03a16c215459119d11f55762d914fe98fcc6d1
thebeen11 <bagus11nugroho@gmail.com>
2019-09-12 15:21:10 +0700

    02-04

---
+++ novice/02-04/latihan/promise.js
@@ -0,0 +1,8 @@
+function storage(nest, name) {
+    return new Promise(resolve => {
+      nest.readStorage(name, result => resolve(result));
+    });
+  }
+  
+  storage(bigOak, "enemies")
+    .then(value => console.log("Got", value));
