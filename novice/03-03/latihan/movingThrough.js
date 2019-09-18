89004fd37c865794759fda77ca2b4137c0864029
thebeen11 <bagus11nugroho@gmail.com>
2019-09-18 21:33:41 +0700

    03-03

---
+++ novice/03-03/latihan/movingThrough.js
@@ -0,0 +1,14 @@
+function talksAbout(node, string) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            if (talksAbout(node.childNodes[i], string)) {
+                return true;
+            }
+        }
+        return false;
+    } else if (node.nodeType == Node.TEXT_NODE) {
+        return node.nodeValue.indexOf(string) > -1;
+    }
+}
+
+console.log(talksAbout(document.body, "book"));
