0e8183636d73b13e93bba91704e9e70a185bd3a3
thebeen11 <bagus11nugroho@gmail.com>
2019-09-19 18:13:51 +0700

    03-04

---
+++ novice/03-04/kasus/even.js
@@ -0,0 +1,35 @@
+$(function () {
+    let rows = 10;
+    let cols = 10;
+    let cells = [];
+    let even = true;
+    function createGrid(root) {
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < cols; j++) {
+                let cell = $('<div>').addClass('cell');
+                cell.attr('data-row', i).attr('data-col', j);
+                cells.push(cell);
+                root.append(cell);
+                
+            }
+        }
+    };
+    function updateState() {
+        cells.forEach(function (cell) {
+            let row = Math.floor(cell.data('row'));
+            let col = Math.floor(cell.data('col'));
+            let sum = row + col;
+            let isEven = sum % 2 == 0;
+            if (isEven === even) {
+                cell.addClass('active');
+            } else {
+                cell.removeClass('active');
+            }
+        });
+        even = !even;
+    }
+    setInterval(updateState, 500);
+
+    let $root = $('.container');
+    createGrid($root);
+});
